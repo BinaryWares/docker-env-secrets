9e296dc6b8bd72ba5748a18ca823fc455b688946
Parthipan Natkunam <natkunamparthipan@gmail.com>
2023-08-26 02:46:26 +0200

    add client directory with a vite setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import success from "./assets/success.png";
+
+function App() {
+  return (
+    <div className="app__container">
+      <img className="center__image" src={success} alt="great success" />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  envDir: "../",
+  plugins: [react()],
+  build: {
+    outDir: "./dist",
+  },
+  publicDir: "./public",
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: process.env.FRONTEND_PORT,
+    proxy: {
+      "/api": {
+        target: `http://${process.env.SERVER_HOST}:${process.env.BACKEND_PORT}`,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
