b1a756ec8ea7a64fe0e4398e3729ca5fad132b24
Parthipan Natkunam <natkunamparthipan@gmail.com>
2023-08-26 03:26:02 +0200

    fix: expose env variables to vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,26 +1,30 @@
 /* eslint-disable no-undef */
-import { defineConfig } from "vite";
+import { resolve } from "path";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  envDir: "../",
-  plugins: [react()],
-  build: {
-    outDir: "./dist",
-  },
-  server: {
-    watch: {
-      usePolling: true,
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode,resolve(__dirname, "../")) };
+  
+  return defineConfig({
+    plugins: [react()],
+    build: {
+      outDir: "./dist",
     },
-    host: true,
-    strictPort: true,
-    port: process.env.FRONTEND_PORT,
-    proxy: {
-      "/api": {
-        target: `http://${process.env.SERVER_HOST}:${process.env.BACKEND_PORT}`,
-        changeOrigin: true,
-        secure: false,
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true,
+      strictPort: true,
+      port: process.env.VITE_FRONTEND_PORT,
+      proxy: {
+        "/api": {
+          target: `http://${process.env.VITE_SERVER_HOST}:${process.env.VITE_BACKEND_PORT}`,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-});
+  });
+};
